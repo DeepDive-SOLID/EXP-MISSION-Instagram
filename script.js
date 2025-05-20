2fcdf6df445e82bcbfe4f27ac09e14d42630fde9
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-20 20:44:52 +0900

    feat: 반응형 뷰포트 추가

---
+++ script.js
@@ -0,0 +1,12 @@
+// function Sidebar() {
+//   const menuToggle = document.getElementById("menuToggle");
+//   const sidebar = document.getElementById("nav-menu");
+
+//   menuToggle.addEventListener("click", () => {
+//     sidebar.classList.toggle("hidden");
+//   });
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+  Sidebar();
+});
